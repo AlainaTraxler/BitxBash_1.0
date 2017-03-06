86fdb0e9342a9216c4726f163ef9785bd60c1ab9
Alaina Traxler <eloavox@gmail.com>
2017-03-06 10:13:26 -0800

    add click listener framework on LoginActivity

--- app/src/main/java/com/epicodus/bitxbit/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/bitxbit/ui/LoginActivity.java
@@ -3,6 +3,7 @@ package com.epicodus.bitxbit.ui;
 import android.support.annotation.BinderThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,11 +13,11 @@ import com.epicodus.bitxbit.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.editText_email) EditText mEditText_Email;
     @BindView(R.id.editText_password) EditText mEditText_password;
     @BindView(R.id.button_signIn) Button mButton_SignIn;
-    @BindView(R.id.textView_signUp) TextView mButton_SignUp;
+    @BindView(R.id.textView_signUp) TextView mTextView_SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +25,16 @@ public class LoginActivity extends AppCompatActivity{
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        mButton_SignIn.setOnClickListener(this);
+        mTextView_SignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mButton_SignIn){
+
+        }else{
+
+        }
     }
 }
