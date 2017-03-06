433f90f1c61d67d526ed9e556d29d44950cc768b
Alaina Traxler <eloavox@gmail.com>
2017-03-06 10:11:08 -0800

    add ButterKnife, be confused by changes to ButterKnife.

--- app/src/main/java/com/epicodus/bitxbit/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/bitxbit/ui/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.epicodus.bitxbit.ui;
 
+import android.support.annotation.BinderThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.epicodus.bitxbit.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity{
+    @BindView(R.id.editText_email) EditText mEditText_Email;
+    @BindView(R.id.editText_password) EditText mEditText_password;
+    @BindView(R.id.button_signIn) Button mButton_SignIn;
+    @BindView(R.id.textView_signUp) TextView mButton_SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
     }
 }
