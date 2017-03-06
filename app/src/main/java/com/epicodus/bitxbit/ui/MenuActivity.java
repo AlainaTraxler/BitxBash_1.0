ae66d5a628952b7bea7d64a8787196122af90230
Alaina Traxler <eloavox@gmail.com>
2017-03-06 14:50:14 -0800

    set up visual framework for main activity

--- app/src/main/java/com/epicodus/bitxbit/ui/MenuActivity.java
+++ app/src/main/java/com/epicodus/bitxbit/ui/MenuActivity.java
@@ -1,9 +1,11 @@
 package com.epicodus.bitxbit.ui;
 
+import android.graphics.Typeface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.epicodus.bitxbit.AuthListenerActivity;
 import com.epicodus.bitxbit.R;
@@ -13,6 +15,8 @@ import butterknife.ButterKnife;
 
 public class MenuActivity extends AuthListenerActivity implements View.OnClickListener{
     @BindView(R.id.FAB_logout) FloatingActionButton mFAB_Logout;
+//    @BindView(R.id.textView_Workout) TextView mTextView_Workout;
+//    @BindView(R.id.textView_Routine) TextView mTextView_Routine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
