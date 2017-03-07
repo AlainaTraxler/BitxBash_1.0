e38945dcb3b41bd79012a67aae20df691b879824
Alaina Traxler <eloavox@gmail.com>
2017-03-07 11:46:08 -0800

    add  filtering for exercises.

---
+++ app/src/main/java/com/epicodus/bitxbit/adapters/FromExerciseAdapter.java
@@ -0,0 +1,68 @@
+package com.epicodus.bitxbit.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.epicodus.bitxbit.R;
+import com.epicodus.bitxbit.models.Exercise;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Alaina Traxler on 3/7/2017.
+ */
+
+public class FromExerciseAdapter extends RecyclerView.Adapter<FromExerciseAdapter.ExerciseViewHolder>{
+    private ArrayList<Exercise> mExercises = new ArrayList<>();
+    private Context mContext;
+
+    //DataTransferInterface dtInterface;
+
+    public FromExerciseAdapter(Context context, ArrayList<Exercise> exercises) {
+        mContext = context;
+        mExercises = exercises;
+    }
+
+    @Override
+    public FromExerciseAdapter.ExerciseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_from_exercise, parent, false);
+        ExerciseViewHolder viewHolder = new ExerciseViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(FromExerciseAdapter.ExerciseViewHolder holder, int position) {
+        holder.bindExercise(mExercises.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExercises.size();
+    }
+
+    public class ExerciseViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.textView_Name) TextView mTextView_ExerciseName;
+        private Context mContext;
+
+        public ExerciseViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindExercise(Exercise exercise) {
+            mTextView_ExerciseName.setText(exercise.getName());
+        }
+
+        public Exercise getExercise(){
+            return mExercises.get(getAdapterPosition());
+        }
+    }
+}
--- app/src/main/java/com/epicodus/bitxbit/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/bitxbit/ui/MainActivity.java
@@ -1,12 +1,17 @@
 package com.epicodus.bitxbit.ui;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -14,27 +19,36 @@ import com.epicodus.bitxbit.AuthListenerActivity;
 import com.epicodus.bitxbit.Constants;
 import com.epicodus.bitxbit.R;
 import com.epicodus.bitxbit.adapters.FirebaseFromExerciseViewHolder;
+import com.epicodus.bitxbit.adapters.FromExerciseAdapter;
 import com.epicodus.bitxbit.models.Exercise;
 import com.firebase.ui.database.FirebaseIndexRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AuthListenerActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener{
     @BindView(R.id.FAB_logout) FloatingActionButton mFAB_Logout;
-
     @BindView(R.id.FAB_Seed) FloatingActionButton mFAB_Seed;
+    @BindView(R.id.FAB_Refresh) FloatingActionButton mFAB_Refresh;
+    @BindView(R.id.searchView) SearchView mSearchView;
     @BindView(R.id.RecyclerView_From) RecyclerView mRecyclerView_From;
     @BindView(R.id.spinner) Spinner mSpinner;
 
     private FirebaseRecyclerAdapter mFromExerciseFirebaseAdapter;
+    private ArrayList<Exercise> mFromExerciseList = new ArrayList<Exercise>();
+    private ArrayList<Exercise> mFilteredFromExerciseList = new ArrayList<Exercise>();
+    private FromExerciseAdapter mFromExerciseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +59,27 @@ public class MainActivity extends AuthListenerActivity implements View.OnClickLi
         mSpinner.setOnItemSelectedListener(this);
         setFromItemTouchListener();
 
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if(s == ""){
+                    setUpFromExerciseAdapter(null);
+                }else{
+                    setUpFromExerciseAdapter(s.toLowerCase());
+                }
+                return false;
+            }
+        });
+
         mFAB_Logout.setOnClickListener(this);
         mFAB_Seed.setOnClickListener(this);
+        mFAB_Refresh.setOnClickListener(this);
+
     }
 
     @Override
@@ -55,40 +88,62 @@ public class MainActivity extends AuthListenerActivity implements View.OnClickLi
             mAuth.signOut();
         }else if(view == mFAB_Seed){
             seedExercisesFromTextFile();
+        }else if(view == mFAB_Refresh){
+            Log.d("Refresh!", "Lovely!");
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
         }
     }
 
-    private void setUpFromExerciseAdapter(){
+    private void fetchExercises(){
         DatabaseReference dbExerciseRef = FirebaseDatabase.getInstance().getReference().child(Constants.DB_EXERCISES);
 
-        DatabaseReference dbKeyRef = FirebaseDatabase.getInstance().getReference().child(Constants.DB_SEARCH).child(Constants.DB_EXERCISES);
-        DatabaseReference dbDataRef = FirebaseDatabase.getInstance().getReference().child(Constants.DB_EXERCISES);
+        dbExerciseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    mFromExerciseList.add(snapshot.getValue(Exercise.class));
+                }
+                setUpFromExerciseAdapter(null);
+            }
 
-//        mFromExerciseFirebaseAdapter = new FirebaseRecyclerAdapter<Exercise, FirebaseFromExerciseViewHolder>
-//                (Exercise.class, R.layout.list_item_from_exercise, FirebaseFromExerciseViewHolder.class, dbExerciseRef) {
-//
-//            @Override
-//            protected void populateViewHolder(FirebaseFromExerciseViewHolder viewHolder, Exercise exercise, int position) {
-//                viewHolder.bindExercise(exercise);
-//            }
-//        };
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-        mFromExerciseFirebaseAdapter = new FirebaseIndexRecyclerAdapter<Exercise, FirebaseFromExerciseViewHolder>
-                (Exercise.class, R.layout.list_item_from_exercise, FirebaseFromExerciseViewHolder.class, dbKeyRef, dbDataRef) {
-                     public void populateViewHolder(FirebaseFromExerciseViewHolder viewHolder, Exercise exercise, int position) {
-                             viewHolder.bindExercise(exercise);
-                         }
-                 };
+            }
+        });
+    }
 
-        mRecyclerView_From.setHasFixedSize(false);
+    private ArrayList<Exercise> filterExercises(String query){
+        mFilteredFromExerciseList.clear();
+
+            for(Exercise exercise : mFromExerciseList){
+                if(query != null) {
+                    if (exercise.getName().toLowerCase().contains(query)) {
+                        mFilteredFromExerciseList.add(exercise);
+                    }
+                }else mFilteredFromExerciseList.add(exercise);
+            }
+
+        Log.d("Returning list", String.valueOf(mFilteredFromExerciseList.size()));
+        return mFilteredFromExerciseList;
+    }
+
+    private void setUpFromExerciseAdapter(String query){
+        mFromExerciseAdapter = new FromExerciseAdapter(getApplicationContext(), filterExercises(query));
+        mRecyclerView_From.setHasFixedSize(true);
         mRecyclerView_From.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView_From.setAdapter(mFromExerciseFirebaseAdapter);
+        mRecyclerView_From.setAdapter(mFromExerciseAdapter);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         if(i == 0){
-            setUpFromExerciseAdapter();
+            if(mFromExerciseList.size() == 0){
+                Log.d(">>>>>", "Triggering first time fetch");
+                fetchExercises();
+            }else{
+                setUpFromExerciseAdapter(null);
+            }
         }else{
             mRecyclerView_From.setAdapter(null);
         }
@@ -109,7 +164,8 @@ public class MainActivity extends AuthListenerActivity implements View.OnClickLi
 
             @Override
             public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
-                mFromExerciseFirebaseAdapter.notifyDataSetChanged();
+                Log.d("Swiped: ", mFilteredFromExerciseList.get(viewHolder.getAdapterPosition()).getName());
+                mFromExerciseAdapter.notifyDataSetChanged();
             }
         };
 
